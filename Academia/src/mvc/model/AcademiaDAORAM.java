0958d48f1fb829ab87c95386724fc61bb13bad5c
Joice Cristina <71676439+joicecriss@users.noreply.github.com>
2024-04-03 23:17:53 -0300

    Criando separação de pastas e algumas classes

---
+++ Academia/src/mvc/model/AcademiaDAORAM.java
@@ -0,0 +1,14 @@
+package mvc.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CAUPT - ALUNOS
+ */
+public class AcademiaDAORAM {
+    
+}
---
+++ Academia/src/mvc/view/GUI.java
@@ -0,0 +1,9 @@
+package mvc.view;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class GUI {
+    Scanner scanner = new Scanner(System.in);
+}
