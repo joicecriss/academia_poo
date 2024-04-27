0c981de1f41de428874ea6b4cf2d2e88dbd46192
Matheus Henrique <matheus.nunes@estudante.iftm.edu.br>
2024-04-27 16:58:32 -0300

    Atualizando para JOptionPane os campos de login

--- Academia/src/mvc/control/Main.java
+++ Academia/src/mvc/control/Main.java
@@ -2,6 +2,8 @@ package mvc.control;
 
 //Importação dos models
 import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import mvc.model.Academia;
 import mvc.model.AcademiaDAO;
 import mvc.model.Pessoa;
@@ -15,30 +17,28 @@ public class Main {
     AcademiaDAO academiaDAO = new AcademiaDAO();
     PessoaDAO pessoaDAO = new PessoaDAO();
     Scanner s = new Scanner(System.in);
+    JFrame frame = new JFrame();
     
     public Main() {
-
         int opcao = 0;
 
         do {
-            gui.menuBoasVindas();
-            opcao = Integer.parseInt(s.nextLine());
+            opcao = gui.menuBoasVindas();
             switch (opcao) {
                 case 1:
                     int login = 0;
                     while(login != 1) {
-                        System.out.println("\n- Digite o email: ");
-                        String email = s.nextLine();
-                        System.out.println("\n- Digite a senha:");
-                        String senha = s.nextLine();
+                        String email = JOptionPane.showInputDialog(frame, "Digite o e-mail: ");
+                        String senha = JOptionPane.showInputDialog(frame, "Digite a senha: ");
                         
                         if(!pessoaDAO.buscaPorLogin(email).equals(email)) {
-                            System.out.println("Email errado, digite novamente!");
+                            JOptionPane.showMessageDialog(frame, "E-mail errado, digite novamente!");
                         } else if (!pessoaDAO.buscaPorSenha(senha).equals(senha)) {
-                            System.out.println("Senha errada, digite novamente!");
+                            JOptionPane.showMessageDialog(frame, "Senha errada, digite novamente!");
                         } else {
                             login = 1;
-                            System.out.println("Login feito");
+                            JOptionPane.showMessageDialog(frame, "Login feito!");
+                            System.out.println("Login feito!");
                             gui.opPessoa();
                         }
                     }                    
