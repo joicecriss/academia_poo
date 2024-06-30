6fd56d8d5d976451f36fd4b3365db85ebb510ce8
Joice Cristina <71676439+joicecriss@users.noreply.github.com>
2024-06-30 19:30:03 -0300

    Update Relatorios.java

--- Academia/src/mvc/model/Relatorios.java
+++ Academia/src/mvc/model/Relatorios.java
@@ -2,7 +2,8 @@ package mvc.model;
 
 /*
 RELATÓRIOS
-=>Exiba o relatório dos alunos adimplentes ao término de um dado mês.
+=>Exiba o relatório dos alunos adimplentes ao término de um dado mês. (Os alunos que estão com mensalidades em dia)
+=>Exiba o relaório dos alunos inadimplentes ao término de um dado mes. (Os alunos que estão com mensalidades em atraso)
 =>Exiba um relatório com toda a movimentação financeira da academia em um dado mês.
 =>Exiba a ficha de treino do aluno.
 */
